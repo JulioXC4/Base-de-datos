e32da08a9eee06ec5443ea1a543860f3e6ec10bd
JulioXC4 <danielc4nt@gmail.com>
2024-10-16 23:30:28 -0500

    actualizacion

--- queries1.sql
+++ queries1.sql
@@ -1,67 +1,67 @@
--- Tabla Empresa
-CREATE TABLE Empresa (
-    codigo_empresa INT PRIMARY KEY IDENTITY(1,1),
-    Nombre NVARCHAR(100),
-    Sector NVARCHAR(50),
-    Correo_electronico NVARCHAR(100),
-    Telefono NVARCHAR(20),
-    Ubicacion NVARCHAR(100)
+-- tabla empresa
+create table empresa (
+    codigo_empresa int primary key identity(1,1),
+    nombre nvarchar(100),
+    sector nvarchar(50),
+    correo_electronico nvarchar(100),
+    telefono nvarchar(20),
+    ubicacion nvarchar(100)
 );
 
--- Tabla Tecnologia
-CREATE TABLE Tecnologia (
-    codigo_tecnologia INT PRIMARY KEY IDENTITY(1,1),
-    Nombre_tecnologia NVARCHAR(100),
-    Descripcion NVARCHAR(255),
-    Requerimientos_Tecnicos NVARCHAR(255)
+-- tabla tecnologia
+create table tecnologia (
+    codigo_tecnologia int primary key identity(1,1),
+    nombre_tecnologia nvarchar(100),
+    descripcion nvarchar(255),
+    requerimientos_tecnicos nvarchar(255)
 );
 
--- Tabla RequerimientoTecnico
-CREATE TABLE RequerimientoTecnico (
-    codigo_requerimiento_tecnico INT PRIMARY KEY IDENTITY(1,1),
-    codigo_vacante INT, -- Asegúrate de que la tabla Vacante existe o la puedes agregar
-    CONSTRAINT FK_RequerimientoTecnico_Vacante FOREIGN KEY (codigo_vacante) 
-    REFERENCES Vacante(codigo_vacante)
+-- tabla requerimientotecnico
+create table requerimientotecnico (
+    codigo_requerimiento_tecnico int primary key identity(1,1),
+    codigo_vacante int, -- asegúrate de que la tabla vacante existe o la puedes agregar
+    constraint fk_requerimientotecnico_vacante foreign key (codigo_vacante) 
+    references vacante(codigo_vacante)
 );
 
--- Tabla TecnologiasRequerimientosTecnicos
-CREATE TABLE TecnologiasRequerimientosTecnicos (
-    codigo_requerimiento_tecnico INT,
-    codigo_tecnologia INT,
-    CONSTRAINT FK_Tecnologias_Requerimientos FOREIGN KEY (codigo_requerimiento_tecnico) 
-    REFERENCES RequerimientoTecnico(codigo_requerimiento_tecnico),
-    CONSTRAINT FK_Tecnologias_Tecnologia FOREIGN KEY (codigo_tecnologia) 
-    REFERENCES Tecnologia(codigo_tecnologia)
+-- tabla tecnologiasrequerimientostecnicos
+create table tecnologiasrequerimientostecnicos (
+    codigo_requerimiento_tecnico int,
+    codigo_tecnologia int,
+    constraint fk_tecnologias_requerimientos foreign key (codigo_requerimiento_tecnico) 
+    references requerimientotecnico(codigo_requerimiento_tecnico),
+    constraint fk_tecnologias_tecnologia foreign key (codigo_tecnologia) 
+    references tecnologia(codigo_tecnologia)
 );
 
-INSERT INTO Empresa (Nombre, Sector, Correo_electronico, Telefono, Ubicacion)
-VALUES 
-('Tech Solutions', 'Tecnología', 'contacto@techsolutions.com', '555-1234', 'Madrid, España'),
-('Innovate Corp', 'Consultoría', 'info@innovatecorp.com', '555-5678', 'Barcelona, España'),
-('HealthPlus', 'Salud', 'info@healthplus.com', '555-4321', 'Sevilla, España'),
-('Green Energy', 'Energía', 'contacto@greenenergy.com', '555-8765', 'Valencia, España'),
-('Data Insight', 'Big Data', 'info@datainsight.com', '555-3456', 'Bilbao, España'),
-('CloudNet', 'Tecnología', 'soporte@cloudnet.com', '555-7890', 'Málaga, España'),
-('AI Innovations', 'Inteligencia Artificial', 'ai@innovations.com', '555-0987', 'Zaragoza, España'),
-('BioTech Labs', 'Biotecnología', 'biotech@labs.com', '555-5432', 'Alicante, España'),
-('SecureIT', 'Ciberseguridad', 'support@secureit.com', '555-2109', 'Murcia, España'),
-('EcoLogica', 'Medio Ambiente', 'info@ecologica.com', '555-6789', 'Granada, España');
+insert into empresa (nombre, sector, correo_electronico, telefono, ubicacion)
+values 
+('tech solutions', 'tecnología', 'contacto@techsolutions.com', '555-1234', 'madrid, españa'),
+('innovate corp', 'consultoría', 'info@innovatecorp.com', '555-5678', 'barcelona, españa'),
+('healthplus', 'salud', 'info@healthplus.com', '555-4321', 'sevilla, españa'),
+('green energy', 'energía', 'contacto@greenenergy.com', '555-8765', 'valencia, españa'),
+('data insight', 'big data', 'info@datainsight.com', '555-3456', 'bilbao, españa'),
+('cloudnet', 'tecnología', 'soporte@cloudnet.com', '555-7890', 'málaga, españa'),
+('ai innovations', 'inteligencia artificial', 'ai@innovations.com', '555-0987', 'zaragoza, españa'),
+('biotech labs', 'biotecnología', 'biotech@labs.com', '555-5432', 'alicante, españa'),
+('secureit', 'ciberseguridad', 'support@secureit.com', '555-2109', 'murcia, españa'),
+('ecologica', 'medio ambiente', 'info@ecologica.com', '555-6789', 'granada, españa');
 
-INSERT INTO Tecnologia (Nombre_tecnologia, Descripcion, Requerimientos_Tecnicos)
-VALUES 
-('Python', 'Lenguaje de programación de alto nivel', 'Conocimiento en librerías de datos'),
-('Java', 'Lenguaje de programación orientado a objetos', 'Experiencia en desarrollo backend'),
-('AWS', 'Plataforma de servicios en la nube', 'Certificación AWS Solutions Architect'),
-('React', 'Librería de JavaScript para construir interfaces de usuario', 'Experiencia con JavaScript y JSX'),
-('Docker', 'Plataforma de contenedores', 'Conocimientos de virtualización y microservicios'),
-('Kubernetes', 'Sistema de orquestación de contenedores', 'Experiencia con Docker y gestión de clusters'),
-('SQL Server', 'Sistema de gestión de bases de datos', 'Conocimientos de consultas SQL y optimización'),
-('TensorFlow', 'Biblioteca de aprendizaje automático', 'Experiencia en modelos de deep learning'),
-('Azure', 'Plataforma de servicios en la nube de Microsoft', 'Certificación en Azure Solutions Architect'),
-('Node.js', 'Entorno de ejecución de JavaScript en el servidor', 'Experiencia en desarrollo full-stack');
+insert into tecnologia (nombre_tecnologia, descripcion, requerimientos_tecnicos)
+values 
+('python', 'lenguaje de programación de alto nivel', 'conocimiento en librerías de datos'),
+('java', 'lenguaje de programación orientado a objetos', 'experiencia en desarrollo backend'),
+('aws', 'plataforma de servicios en la nube', 'certificación aws solutions architect'),
+('react', 'librería de javascript para construir interfaces de usuario', 'experiencia con javascript y jsx'),
+('docker', 'plataforma de contenedores', 'conocimientos de virtualización y microservicios'),
+('kubernetes', 'sistema de orquestación de contenedores', 'experiencia con docker y gestión de clusters'),
+('sql server', 'sistema de gestión de bases de datos', 'conocimientos de consultas sql y optimización'),
+('tensorflow', 'biblioteca de aprendizaje automático', 'experiencia en modelos de deep learning'),
+('azure', 'plataforma de servicios en la nube de microsoft', 'certificación en azure solutions architect'),
+('node.js', 'entorno de ejecución de javascript en el servidor', 'experiencia en desarrollo full-stack');
 
-/* INSERT INTO RequerimientoTecnico (codigo_vacante)
-VALUES 
+/* insert into requerimientotecnico (codigo_vacante)
+values 
 (1),
 (2),
 (3),
@@ -74,15 +74,15 @@ VALUES
 (10);
  */
 
-/* INSERT INTO TecnologiasRequerimientosTecnicos (codigo_requerimiento_tecnico, codigo_tecnologia)
-VALUES 
-(1, 1), -- Python requerido en la vacante 1
-(2, 2), -- Java requerido en la vacante 2
-(3, 3), -- AWS requerido en la vacante 3
-(4, 4), -- React requerido en la vacante 4
-(5, 5), -- Docker requerido en la vacante 5
-(6, 6), -- Kubernetes requerido en la vacante 6
-(7, 7), -- SQL Server requerido en la vacante 7
-(8, 8), -- TensorFlow requerido en la vacante 8
-(9, 9), -- Azure requerido en la vacante 9
-(10, 10); -- Node.js requerido en la vacante 10 */
+ /* insert into tecnologiasrequerimientostecnicos (codigo_requerimiento_tecnico, codigo_tecnologia)
+values 
+(1, 1), -- python requerido en la vacante 1
+(2, 2), -- java requerido en la vacante 2
+(3, 3), -- aws requerido en la vacante 3
+(4, 4), -- react requerido en la vacante 4
+(5, 5), -- docker requerido en la vacante 5
+(6, 6), -- kubernetes requerido en la vacante 6
+(7, 7), -- sql server requerido en la vacante 7
+(8, 8), -- tensorflow requerido en la vacante 8
+(9, 9), -- azure requerido en la vacante 9
+(10, 10); -- node.js requerido en la vacante 10 */
